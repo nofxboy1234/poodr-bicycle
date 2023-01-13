978efcf816eeb84caf4ed3f53b11618b41d07ef4
Dylan Palmboom <nofxboy1234@gmail.com>
2023-01-13 12:58:58 +1000

    Add gear_blinkered to test private accessor method Gear.cog

---
+++ gear_blinkered.rb
@@ -0,0 +1,23 @@
+require 'pry-byebug'
+
+class Gear
+  private
+
+  attr_reader :chainring, :cog
+
+  public
+
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
+  end
+end
+
+class Blinkered
+  def cog(gear)
+    gear.cog
+  end
+end
+
+binding.pry
+puts Blinkered.new.cog(Gear.new(54, 11))
